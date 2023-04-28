a35293a1f5a8591ec7ddd484129e4d2e142e4e3a
Julio Camargo <jcamargo@hotmail.co.uk>
2023-04-28 11:47:41 +0100

    connecting all

--- src/server.js
+++ src/server.js
@@ -1,34 +1,36 @@
 import express from 'express';
+import { db, connectToDb } from './db.js';
 
-  let articlesInfo = [
-    {
-      name: 'learn-react',
-      upvotes: 0,
-      comments:[],
-    },
-    {
-      name: 'learn-node',
-      upvotes: 0,
-      comments:[],
-    },
-    {
-      name: 'my-thoughts-on-resumes',
-      upvotes: 0,
-      comments:[],
-    },
-  ]
-    
 
 const app = express();
 app.use(express.json());
 
 
-app.put('/api/articles/:name/upvote', (req, res) => {
+app.get('/api/articles/:name', async (req, res) => {
   const { name } = req.params;
-  const article = articlesInfo.find((article) => article.name === name);
+
+  const article = await db.collection('articles').findOne({ name });
 
   if (article) {
-    article.upvotes += 1;
+    res.status(200).json(article);
+
+  } else {
+    res.status(404).send('Article not found');
+  }
+});
+
+app.put('/api/articles/:name/upvote', async (req, res) => {
+  const { name } = req.params;
+
+
+  await db.collection('articles').updateOne({ name }, {
+    '$inc': {
+      upvotes: 1,
+    },
+  });
+  const article = await db.collection('articles').findOne({ name });
+
+  if (article) {
     res.send(`${name} now has ${article.upvotes} upvotes`);
   }
   else {
@@ -36,12 +38,23 @@ app.put('/api/articles/:name/upvote', (req, res) => {
   }
 });
 
-app.post('/api/articles/:name/comments', (req, res) => {
+
+
+app.post('/api/articles/:name/comments', async (req, res) => {
   const { name } = req.params;
   const { postedBy, text } = req.body;
-  const article = articlesInfo.find((article) => article.name === name);
+
+  await db.collection('articles').updateOne({ name }, {
+    '$push': {
+      comments: {
+        postedBy,
+        text,
+      },
+    },
+  });
+  const article = await db.collection('articles').findOne({ name });
+
   if (article) {
-    article.comments.push({postedBy, text});
     res.status(200).send(article.comments);
   }
   else {
@@ -49,9 +62,9 @@ app.post('/api/articles/:name/comments', (req, res) => {
   }
 });
 
-
-
-
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000');
+connectToDb(() => {
+  console.log('Successfully connected to database')
+  app.listen(5000, () => {
+    console.log('Server is listening on port 5000');
+  });
 });
