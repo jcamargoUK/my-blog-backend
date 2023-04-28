cb5448724140f6c2dcfd798edeb2a67962d012a4
Julio Camargo <jcamargo@hotmail.co.uk>
2023-04-28 10:14:00 +0100

    added database

--- src/server.js
+++ src/server.js
@@ -4,14 +4,17 @@ import express from 'express';
     {
       name: 'learn-react',
       upvotes: 0,
+      comments:[],
     },
     {
       name: 'learn-node',
       upvotes: 0,
+      comments:[],
     },
     {
       name: 'my-thoughts-on-resumes',
       upvotes: 0,
+      comments:[],
     },
   ]
     
@@ -23,6 +26,7 @@ app.use(express.json());
 app.put('/api/articles/:name/upvote', (req, res) => {
   const { name } = req.params;
   const article = articlesInfo.find((article) => article.name === name);
+
   if (article) {
     article.upvotes += 1;
     res.send(`${name} now has ${article.upvotes} upvotes`);
@@ -30,9 +34,23 @@ app.put('/api/articles/:name/upvote', (req, res) => {
   else {
     res.status(404).send('Article not found');
   }
-  
-
 });
+
+app.post('/api/articles/:name/comments', (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+  const article = articlesInfo.find((article) => article.name === name);
+  if (article) {
+    article.comments.push({postedBy, text});
+    res.status(200).send(article.comments);
+  }
+  else {
+    res.status(404).send('Article not found');
+  }
+});
+
+
+
 
 app.listen(5000, () => {
   console.log('Server is listening on port 5000');
